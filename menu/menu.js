04fcec1ee1ff20d8f10d1f90aa62fd1893f4f4db
Mohamed Abdelmenam <mabdelmenam@gmail.com>
2019-10-21 14:09:29 -0700

    *Completed menu for all 4 sections of Menu Fixed JS function, from getting className to getting Id of ' div ' Added active selector to menu.css for menu-navbar

--- menu/menu.js
+++ menu/menu.js
@@ -2,10 +2,12 @@ function loadCategory(currentDiv, file){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function(){
         if(this.readyState == 4 && this.status == 200){
+            console.log(xhttp.responseText);
             document.getElementById(currentDiv).innerHTML = xhttp.responseText;
         }
-    }
+    };
 
-    xhttp.open('GET', file, true);
+    xhttp.open('GET', file, true); 
+    //console.log(xhttp.send());
     xhttp.send();
 }
