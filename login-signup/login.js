374b6220ded380918ac2edbfa5834d52484174b4
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2019-10-23 20:00:01 -0400

    Fixed pathing between files

--- login-signup/login.js
+++ login-signup/login.js
@@ -1,3 +1,8 @@
-function login(){
-    window.location.replace('./login-signup/loginPage.html');
+function login(id) {
+  if (id == "login") {
+    window.location.replace("/delivery-project/login-signup/loginPage.html");
+  } else if (id == "title") {
+    window.location.replace("/delivery-project/index.html");
+  }
+  console.log("inside");
 }
