8b3ae5dea6e64c390d89a85c3b60626ae1aaeaf4
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2019-10-23 22:30:48 -0400

    *Completed Login Page design Added the '+' icon to menu, hidden until functional

--- menu/menu.js
+++ menu/menu.js
@@ -1,13 +1,16 @@
-function loadCategory(currentDiv, file){
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function(){
-        if(this.readyState == 4 && this.status == 200){
-            console.log(xhttp.responseText);
-            document.getElementById(currentDiv).innerHTML = xhttp.responseText;
-        }
-    };
+function loadCategory(currentDiv, file) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      console.log(xhttp.responseText);
+      document.getElementById(currentDiv).innerHTML = xhttp.responseText;
+    }
+  };
 
-    xhttp.open('GET', file, true); 
-    //console.log(xhttp.send());
-    xhttp.send();
+  xhttp.open("GET", file, true);
+  //console.log(xhttp.send());
+  xhttp.send();
+}
+function plusButton() {
+  var plusBtn = document.getElementsByClassName("plus-btn");
 }
