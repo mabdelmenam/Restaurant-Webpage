c9c3f141b70a906682fbdee6c354cd5ec2ba5e7c
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2019-10-23 23:02:00 -0400

    Completed design of Signup Page

--- login-signup/login.js
+++ login-signup/login.js
@@ -3,6 +3,7 @@ function login(id) {
     window.location.replace("/delivery-project/login-signup/loginPage.html");
   } else if (id == "title") {
     window.location.replace("/delivery-project/index.html");
+  } else if (id == "sign-up-btn") {
+    window.location.replace("/delivery-project/login-signup/sign-up.html");
   }
-  console.log("inside");
 }
