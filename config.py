404ca749030bb6912794d5f997ac5555a97256b8
Mohamed Abdelmenam <mabdelmenam@gmail.com>
2019-12-21 14:05:10 -0800

    Revert "Moved files around" reverts commit 730055c0f0af8c997ceca2957eefe253190569da.

--- config.py
+++
@@ -1,7 +0,0 @@
-import yaml 
-
-db = yaml.safe_load(open('db.yaml'))
-MYSQL_HOST = db['mysql_host']
-MYSQL_USER = db['mysql_user']
-MYSQL_PASSWORD = db['mysql_password']
-MYSQL_DB = db['mysql_database']
