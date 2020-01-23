9f270180886602cbe71cd0b85a629b7af5a75551
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2020-01-23 04:21:49 -0500

    In process of creating modal for food itmes

--- python_wsgi/static/js/menu.js
+++ python_wsgi/static/js/menu.js
@@ -1,3 +1,8 @@
+var arr = [];
+var foodDisplay = document.getElementById("modal-food-heading");
+var modal = document.getElementsByClassName('modal');
+var closebtn = document.getElementById('modal-close');
+
 function loadCategory(currentDiv, file) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
@@ -8,42 +13,48 @@ function loadCategory(currentDiv, file) {
   };
 
   xhttp.open("GET", file, true);
-  //console.log(xhttp.send());
   xhttp.send();
 }
 function plusButton() {
-  //create condition here to run, based on true or false of user logged in or not
-  //if user is logged in then continue on with the script and create the buttons,
-  // if not then do nothing
-  /**
-   * if valid is 1 in json, then run this function
-   */
-  //   <button class="plus-btn"><i class="fa fa-plus"></i></button>
   var file = "http://localhost:8000/check_session";
 
   var xhttp = new XMLHttpRequest();
 
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
-      console.log('MENU RESPONSE TEXT: ', this.responseText);
       var session_active = JSON.parse(this.responseText);
+
       if (session_active.active == 1) { //BEGIN IF STATEMENT
         var paragraphs = document.querySelectorAll("[id='food']");
 
         for (var i = 0; i < paragraphs.length; i++) {// BEGIN FOR LOOP
+          var foodName = paragraphs[i].childNodes[1].innerHTML;
+          arr.push(foodName);
+          //console.log(foodName + " " + i);
           var buttonTag = document.createElement('button');
           buttonTag.className = 'plus-btn';
-          //buttonTag.addEventListener("click", modalControl(), false);
 
           var iTag = document.createElement('i');
           iTag.className = 'fa fa-plus';
 
           buttonTag.appendChild(iTag);
-
           paragraphs[i].insertBefore(buttonTag, paragraphs[i].childNodes[2]);
-        } // END FOR LOOP
 
+          buttonTag.onclick = function () {
+            modal[0].style.display = 'block';
+          }
+        } // END FOR LOOP
       }// END IF STATEMENT
+      var buttonClass = document.getElementsByClassName('plus-btn');
+      console.log("Button Class Length: ", buttonClass.length);
+      x = buttonClass.length;
+      console.log("X: " + x);
+      for (var i = 0; i < buttonClass.length; i++) {
+        console.log(buttonClass[i]);
+        buttonClass[i].onclick = (function (i) {
+          food(arr[i]);
+        })(i);
+      }
     }
   };
   xhttp.open("GET", file, true);
@@ -52,28 +63,14 @@ function plusButton() {
 }
 window.onload = plusButton();
 
-
-
-
-//function modalControl() {
-var btn = document.getElementsByClassName('plus-btn');
-if (btn) {
-  console.log("exsists");
+/*document.getElementById('cell' + i).onclick = (function (i) {
+  return function () { select(i); }
+})(i);*/
+function food(foodName) {
+  console.log("yo: ", foodName);
+  foodDisplay.innerHTML = foodName;
 }
-var modal = document.getElementById('modal');
-var closebtn = document.getElementById('modal-close');
-console.log("clicked1");
-btn.onclick = function () {
-  console.log("clicked");
-  modal.style.display = 'block';
-}
+//modal functionality
 closebtn.onclick = function () {
-  modal.style.display = 'none';
+  modal[0].style.display = 'none';
 }
-window.onclick = function () {
-  if (event.target == modal) {
-    modal.style.display = 'none';
-  }
-}
-//}
-
