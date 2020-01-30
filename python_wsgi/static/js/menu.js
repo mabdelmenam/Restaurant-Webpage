792ad08de412ef454cc71d781d0346af734a3a63
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2020-01-29 19:21:01 -0500

    -Placed all images in static/images directory -Modal food, price displayed -Textarea for food instructions -Quantity Counter -Design 80 Done -Next step is to add 'Add' button and functionality

--- python_wsgi/static/js/menu.js
+++ python_wsgi/static/js/menu.js
@@ -1,13 +1,21 @@
-var arr = [];
+var names = [];
+var prices = [];
 var foodDisplay = document.getElementById("modal-food-heading");
+var priceDisplay = document.getElementById("modal-food-price");
 var modal = document.getElementsByClassName('modal');
 var closebtn = document.getElementById('modal-close');
 
+/**Loading into different HTML files */
 function loadCategory(currentDiv, file) {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
+      names = [];
+      prices = [];
       window.onload = plusButton();
+      setTimeout(function () { //setting a timer waiting for plusButton to completely finish
+        window.onload = food();
+      }, 300);
       document.getElementById(currentDiv).innerHTML = xhttp.responseText;
     }
   };
@@ -15,6 +23,8 @@ function loadCategory(currentDiv, file) {
   xhttp.open("GET", file, true);
   xhttp.send();
 }
+
+/**Plus Button functionality, and checking current session status */
 function plusButton() {
   var file = "http://localhost:8000/check_session";
 
@@ -29,7 +39,9 @@ function plusButton() {
 
         for (var i = 0; i < paragraphs.length; i++) {// BEGIN FOR LOOP
           var foodName = paragraphs[i].childNodes[1].innerHTML;
-          arr.push(foodName);
+          var foodPrice = paragraphs[i].getElementsByTagName('b')[0].innerHTML;
+          prices.push(foodPrice)
+          names.push(foodName);
           //console.log(foodName + " " + i);
           var buttonTag = document.createElement('button');
           buttonTag.className = 'plus-btn';
@@ -46,34 +58,59 @@ function plusButton() {
     }
   };
   xhttp.open("GET", file, true);
+  //xhttp.setHeader("Set-Cookie", "HttpOnly;Secure;SameSite=Strict");
   xhttp.withCredentials = true;
   xhttp.send(null);
 }
 window.onload = plusButton();
+window.onload = food();
 
+
+/**Creating onclick functions for plus buttons in buttonClass, and modal functionality */
 var buttonClass = document.getElementsByClassName('plus-btn');
 
-setTimeout(function () {
+function food() {
+  setTimeout(function () {
+    console.log(prices)
 
-  var handler = function (index) {
-    // var u = document.getElementById(index);
-    modal[0].style.display = 'block';
-    foodDisplay.innerHTML = arr[index];
-    //console.log(u);
-    console.log();
-  };
-
-
-  for (var i = 0; i < buttonClass.length; i++) {
-    buttonClass[i].onclick = (function () {
-      var currI = i;
-      return function () {
-        handler(currI);
-      }
-    })();
-  }
-}, 15);
+    var handler = function (index) {
+      console.log("Inside Handler function");
+      modal[0].style.display = 'block';
+      foodDisplay.innerHTML = names[index];
+      priceDisplay.innerHTML = prices[index];
+      console.log();
+    };
+    console.log("Before For loop");
+    console.log("ButtonClass: ", buttonClass);
+    console.log("Button Class Length: ", buttonClass.length);
+    for (var i = 0; i < buttonClass.length; i++) {
+      buttonClass[i].onclick = (function () {
+        var currI = i;
+        return function () {
+          handler(currI);
+        }
+      })();
+    }
+  }, 50);
+}
 //modal functionality
 closebtn.onclick = function () {
   modal[0].style.display = 'none';
+}
+
+/**Quantity counter */
+var counter = 0;
+function foodQuantity(id) {
+  var quantity = document.getElementById("quantity");
+  if (id == "quantity-plus") {
+    counter++;
+  } else {
+    counter--;
+    if (counter < 0) {
+      counter = 0;
+      quantity.innerHTML = counter;
+      return;
+    }
+  }
+  quantity.innerHTML = counter;
 }
