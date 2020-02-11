723b540b94fa068ba8a0ffe57e2af06610c350e4
mabdelmenam <34500151+mabdelmenam@users.noreply.github.com>
2020-02-11 01:38:32 -0500

    Creating dropdown for user to see their current order Styling in process

--- python_wsgi/static/js/menu.js
+++ python_wsgi/static/js/menu.js
@@ -103,41 +103,6 @@ closebtn.onclick = function () {
   modalReset();
 }
 
-//Add Button
-var addButton = document.getElementById("add-button");
-addButton.onclick = function () {
-  var foodDB = foodDisplay.innerHTML;
-  var finalPriceDB = num2.toFixed(2);
-  var quantityDB = parseInt(document.getElementById("quantity").innerHTML);
-  var instructionsDB = document.getElementById('food-instructions').value;
-
-  var info = {
-    food: foodDB,
-    price: finalPriceDB,
-    quantity: quantityDB,
-    food_instructions: instructionsDB
-  };
-  console.log(info.price);
-  var finalData = JSON.stringify(info);
-  var file = "http://localhost:8000/food_database";
-
-  var xhttp = new XMLHttpRequest();
-
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      console.log("RESPONSE: ", this.responseText);
-    }
-  };
-
-  xhttp.open("POST", file, true);
-  xhttp.setRequestHeader("Content-Type", "application/json");
-  xhttp.send(finalData);
-
-  //modalReset();
-}
-
-
-
 function modalReset() {
   modal[0].style.display = 'none';
   document.getElementById("food-instructions").value = "";
--- python_wsgi/static/js/quantity.js
+++ python_wsgi/static/js/quantity.js
@@ -23,3 +23,48 @@ function foodQuantity(id) {
     quantity.innerHTML = counter;
     console.log(num2);
 }
+
+
+//Add Button
+var addButton = document.getElementById("add-button");
+addButton.onclick = function () {
+    var foodDB = foodDisplay.innerHTML;
+    var finalPriceDB = num2.toFixed(2);
+    var quantityDB = parseInt(document.getElementById("quantity").innerHTML);
+    var instructionsDB = document.getElementById('food-instructions').value;
+
+    var info = {
+        food: foodDB,
+        price: finalPriceDB,
+        quantity: quantityDB,
+        food_instructions: instructionsDB
+    };
+    console.log(info.price);
+    var finalData = JSON.stringify(info);
+    var file = "http://localhost:8000/food_database";
+
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("RESPONSE: ", this.responseText);
+        }
+    };
+
+    xhttp.open("POST", file, true);
+    xhttp.setRequestHeader("Content-Type", "application/json");
+    xhttp.send(finalData);
+
+    modalReset();
+}
+
+function dropdown() {
+    var dropdown = document.getElementById("dropdown-content");
+    //var dropdown2 = dropdown.document.querySelectorAll('h1,a');
+    console.log(dropdown);
+    //console.log(dropdown.classList);
+    var x = dropdown.style.display = 'block';
+    dropdown.classList.toggle(x);
+
+    console.log("clicked");
+}
